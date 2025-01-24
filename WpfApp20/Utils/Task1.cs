55265bad0059e559820947a7fc04b67f6396c571
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2025-01-24 22:42:59 +0300

    Добавьте файлы проекта.

---
+++ WpfApp20/Utils/Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Utils
+{
+    public class Task1
+    {
+        public static double Calculate(Dictionary<string, double> vars)
+        {
+            double x = vars["x"];
+            double y = vars["y"];
+            double f = vars["f"];
+            return Math.Exp(x) * Math.Sin(f) / Math.Log(3.8 * y + f);
+        }
+    }
+}
